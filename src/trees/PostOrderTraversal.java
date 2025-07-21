130d1b73da5d86ada8dc18724a44e3abb3584700
saredd <suryaprathap.reddy@planonsoftware.com>
2025-07-21 12:08:58 +0530

    added some tree traversal based from takeuforward sheet

---
+++ src/trees/PostOrderTraversal.java
@@ -0,0 +1,30 @@
+package trees;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class PostOrderTraversal {
+    public List<Integer> postorder(TreeNode root) {
+        List<Integer> postOrderResult = new ArrayList<>();
+        TreeNode current = root;
+        TreeNode lastVisited = null;
+        Stack<TreeNode> nodeStack = new Stack<>();
+        while (!nodeStack.isEmpty() || current != null) {
+            if (current != null) {
+                nodeStack.push(current);
+                current = current.left;
+            } else {
+                TreeNode peekNode = nodeStack.peek();
+                if (peekNode.right != null && peekNode.right != lastVisited) {
+                    current = peekNode.right;
+                } else {
+                    postOrderResult.add(peekNode.data);
+                    lastVisited = nodeStack.pop();
+                }
+            }
+
+        }
+        return postOrderResult;
+    }
+}
